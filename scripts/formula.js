864624f04755bc581c2334f49948395b323ebde4
= <=>
2021-11-13 10:24:37 +0000

    Refactor temperature conversion functions

--- scripts/formula.js
+++ scripts/formula.js
@@ -1,7 +1,12 @@
-function celsiusToFahrenheit(celsius) {
-  return celsius * 1.8 + 32;
-}
+// Temperature conversion formula
+const celsiusToFahrenheit = (celsius) => { return celsius * 1.8 + 32 };
 
-function fahrenheitToCelsius(fahrenheit) {
-  return (fahrenheit - 32) / 1.8;
-}
+const fahrenheitToCelsius = (fahrenheit) => { return (fahrenheit - 32) / 1.8 };
+
+const celsiusToKelvin = (celsius) => { return celsius + 273.15 };
+
+const kelvinToCelsius = (kelvin) => { return kelvin - 273.15 };
+
+const fahrenheitToKelvin = (fahrenheit) => { return (fahrenheit + 459.67) / 1.8 };
+
+const kelvinToFahrenheit = (fahrenheit) => { return fahrenheit * 1.8 - 459.67 };
